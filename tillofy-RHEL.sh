df2eab6df8531ff57694fab9896a1a086a314aff
root <root@Darius.iconos.be>
2017-03-13 13:42:24 +0100

    change theme command, script needs to run twice tho. will need to fix that at some point

--- tillofy-RHEL.sh
+++ tillofy-RHEL.sh
@@ -6,8 +6,16 @@ sudo yum -y update
 sudo yum -y install ${packages}
 sudo yum -y autoremove
 
+#zshscript="$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+
+#sudo sed -e "s/ZSH_THEME=\"robbyrussell\"/ZSH_THEME=\"agnoster\"" ~/.zshrc > ~/.zshrcOLD
+#sudo mv ~/.zshrcOLD ~/.zshrc
+
 sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
-sudo su
-usermod -s /bin/zsh $username
+
+sed -i 's/"robbyrussell"/"agnoster"/' ~/.zshrc
+
+
+sudo su -c "usermod -s /bin/zsh $username"
 
 exit   
